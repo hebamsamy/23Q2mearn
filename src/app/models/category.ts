59411c2368e550e3e354cf52a187b8d025196c37
hebamsamy <hebasamy655@gmail.com>
2023-01-18 15:13:43 +0200

    fix string issue

--- src/app/models/category.ts
+++ src/app/models/category.ts
@@ -7,19 +7,19 @@ interface ICategory {
 class test {
     products: ICategory[] = [
         {
-          "id": 1,
-          "name": "Laptops",
-          "desciption": "This is description"
+          id: 1,
+          name: "Laptops",
+          desciption: "This is description"
         },
         {
-          "id": 2,
-          "name": "Tablets",
-          "desciption": "This is description"
+          id: 2,
+          name: "Tablets",
+          desciption: "This is description"
         },
         {
-          "id": 3,
-          "name": "Mobiles",
-          "desciption": "This is description"
+          id: 3,
+          name: "Mobiles",
+          desciption: "This is description"
         }
       ]
 }
